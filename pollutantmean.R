f85fcec644027134f465932455798f78800063f4
volstep <volstep@hotmail.com>
2017-12-11 03:04:57 +0200

    Add file _R

---
+++ pollutantmean.R
@@ -0,0 +1,9 @@
+
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+  data = numeric()
+  for (i in id) {
+    newRead = read.csv(paste(directory, "/", formatC(i, width = 3, flag = "0"),".csv", sep = ""))
+    data = c(data, newRead[[pollutant]])
+  }
+  return(mean(data, na.rm = TRUE))
+}
